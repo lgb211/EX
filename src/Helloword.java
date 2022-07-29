8284a549b3708e59516f63fa746d6efa99747da7
lgb211 <rkdqls0122@naver.com>
2022-07-29 17:44:50 +0900

    This is Commit Message

---
+++ src/Helloword.java
@@ -0,0 +1,25 @@
+
+public class Helloword {
+public static void main(String[] args) {
+	System.out.println("hello word");
+	int n= 3;
+	Helloword hello = new Helloword();
+	hello.dan(n);
+//	System.out.println(n*1);
+//	System.out.println(n*2);
+//	System.out.println(n*3);
+//	System.out.println(n*4);
+//	System.out.println(n*5);
+//	System.out.println(n*6);
+//	System.out.println(n*7);
+//	System.out.println(n*8);
+//	System.out.println(n*9);
+//	
+}
+ 	public void dan(int n) {
+ 	
+ 		for(int i=1;i<10;i++) {
+ 			System.out.println(n*i);
+ 		}
+ 	}
+}
