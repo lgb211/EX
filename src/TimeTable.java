29890899056f48b91ca389511ddfc7fccce73979
lgb211 <rkdqls0122@naver.com>
2022-07-29 18:41:12 +0900

    This is TimesTable

--- src/TimeTable.java
+++ src/TimeTable.java
@@ -4,9 +4,15 @@ public class TimeTable {
 public static void main(String[] args) {
 	Scanner scan= new Scanner(System.in);
 	int j=scan.nextInt();
-	for(int i=1;i<10;i++) {
+	if(j>=10) {
+		System.out.println("입력받은 값이 9보다 큽니다");
+	}else if(j<2) {
+		System.out.println("입력받은 값이 2보다 작습니다");
+	}else {
+		for(int i=1;i<10;i++) {
 		System.out.println(j+"*"+i+"="+j*i);
+		}
+	}
 	}
 }
 
-}
